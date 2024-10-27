8ef93840fcf7ddc2807f9ec7e0686c3d4b52b924
lucaswerner90 <wernerlucas12@gmail.com>
2024-10-27 21:03:15 +0100

    Initial model using Keras

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+import pandas as pd
+from preprocess import preprocess_data
+
+if __name__ == "__main__":
+    train_df = pd.read_csv(os.path.join(os.getcwd(),'data','train.csv'))
+    preprocess_data(train_df)
---
+++ preprocess.py
@@ -0,0 +1,3 @@
+import pandas as pd
+def preprocess_data(df:pd.DataFrame):
+    pass
